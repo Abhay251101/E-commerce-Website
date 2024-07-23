efc449af66b2fb37243b75af3a3d7a0b61be6a57
Abhay251101 <abhaypratap2424@gmail.com>
2024-07-23 22:11:25 +0530

    first commit

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="mylogo2.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <form class="d-flex">
+        <a class="btn btn-outline-success mx-2" type="submit" href="signup.php">Sign Up</button></a>
+       <a class="btn btn-outline-primary mx-2" type="submit" href="login.php">LogIn</button></a> 
+       <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">LogOut</button></a>
+      </form>
+    </div>
+  </div>
+</nav>
